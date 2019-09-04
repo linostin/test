a0be541e7709b83dfeb2fa2885d5bb3622238416
linostin <lifesins@gmail.com>
2019-09-04 17:18:21 +0300

    вторая задача из книги

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,4 +1,19 @@
-for (let symvol="#";symvol.length < 8;symvol = symvol + "#") {
-    console.log(symvol);
-}
+//for (let symvol="#";symvol.length < 8;symvol = symvol + "#") {
+//    console.log(symvol);
 
+//}
+
+for (let chet = 1; chet <= 100; chet += 1) {
+    if (chet % 3 == 0) {
+        if (chet % 5 == 0) {
+            console.log("FuzzyBuzzy35");
+        }
+        console.log("Fuzzy3");
+    }
+    else if (chet % 5 == 0) {
+        console.log("Buzzy5");
+    }
+    else {
+        console.log(chet); 
+    }
+}
