0d9819559cb58bcb36477a11ba1991e78fea226b
linostin <lifesins@gmail.com>
2019-10-08 22:37:34 +0300

    ok

--- scripts/objects.js
+++ scripts/objects.js
@@ -5,6 +5,7 @@ for (let symvol="#";symvol.length < 8;symvol = symvol + "#") {
 }
 */
 
+/*задача 2
 for (let chet = 1; chet <= 100; chet += 1) {
     if (chet % 3 == 0) {
         if (chet % 5 == 0) {
@@ -19,3 +20,18 @@ for (let chet = 1; chet <= 100; chet += 1) {
         console.log(chet); 
     }
 }
+*/
+/*
+let day1 = {
+    squirrel: false,
+    events: ["работал", "трогал дерево", "ел пиццу", "бегал"] 
+};
+console.log(day1.squirrel);
+*/
+
+let journal = [];
+function addEntry(events, squirrel) {
+    journal.push({events, squirrel});
+}
+addEntry(["работал", "трогал дерево", "ел лазанью"], false);
+
