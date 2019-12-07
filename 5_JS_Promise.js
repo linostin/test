ac9916ac43c00e365ed7c524d3dbddaabcb8578b
linostin <lifesins@gmail.com>
2019-12-07 15:36:21 +0300

    6 lesson

--- 5_JS_Promise.js
+++ 5_JS_Promise.js
@@ -1,4 +1,4 @@
-console.log("request data...");
+//console.log("request data...");
 
 // setTimeout(() => {
 //     console.log("Preparin data...")
@@ -18,30 +18,47 @@ console.log("request data...");
 
 //с помощью Promise ниже
 
-const p = new Promise(function(resolve, reject) {
-  setTimeout(() => {
-    console.log("Preparin data...");
-    const backendData = {
-      server: "aws",
-      port: 2000,
-      staatus: "working"
-    };
-    resolve(backendData);
-  }, 2000);
+// const p = new Promise(function(resolve, reject) {
+//   setTimeout(() => {
+//     console.log("Preparin data...");
+//     const backendData = {
+//       server: "aws",
+//       port: 2000,
+//       staatus: "working"
+//     };
+//     resolve(backendData);
+//   }, 2000);
+// });
+
+// p.then((data) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       data.modified = true;
+//       resolve(data);
+//     }, 2000);
+//   });
+
+// //   p2.then(clientData => {
+// //       console.log("Data received", clientData)
+// //   })
+
+// }).then(clientData => {
+//     console.log("Data received", clientData)
+// })
+
+const sleep = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), ms);
+  });
+};
+
+// sleep(2000).then(() => console.log("After 2 seconds"));
+// sleep(3000).then(() => console.log("After 3 seconds"));
+
+Promise.all([sleep(2000), sleep(5000)]).then(() => {
+  console.log("All promises");
 });
 
-p.then((data) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      data.modified = true;
-      resolve(data);
-    }, 2000);
-  });
-
-//   p2.then(clientData => {
-//       console.log("Data received", clientData)
-//   })
-
-}).then(clientData => {
-    console.log("Data received", clientData)
-})
+Promise.race([sleep(2000), sleep(5000)]).then(() => {
+  console.log("Race promises");
+});
