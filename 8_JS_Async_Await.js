5240ce7190fda860ece800e58d6914ba5f4c72e7
linostin <lifesins@gmail.com>
2019-12-08 01:41:13 +0300

    8 lesson

--- 8_JS_Async_Await.js
+++ 8_JS_Async_Await.js
@@ -1,1 +1,15 @@
-console.log("привет!");
+
+
+const delay = ms => {
+    return new Promise(r => setTimeout(() => r(), ms))
+}
+
+//delay(2000).then(() => console.log("привет!"))
+
+const url = "https://jsonplaceholder.typicode.com/todos"
+
+function fetchTodos() {
+    delay(2000).then(() => {
+        fetch(url).then(response => response.json()) 
+    })
+}
