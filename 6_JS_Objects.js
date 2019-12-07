7941f3f1775ca46b53c964b59a6492de7639802c
linostin <lifesins@gmail.com>
2019-12-07 15:55:01 +0300

    6 lesson end

--- 6_JS_Objects.js
+++ 6_JS_Objects.js
@@ -1,1 +1,40 @@
-console.log("привет!");
+const person = Object.create(
+  {
+    calculaateAge() {
+      console.log("Age", new Date().getFullYear() - this.birthYear);
+    }
+  },
+  {
+    name: {
+      value: "Viktor",
+      enumerable: true,
+      writable: true,
+      configurable: true
+    },
+    birthYear: {
+      value: 1989,
+      enumerable: false,
+      writable: false,
+      configurable: false
+    },
+    age: {
+      get() {
+        return new Date().getFullYear() - this.birthYear;
+      },
+      set(value) {
+        console.log("Set age", value);
+      }
+    }
+  }
+);
+
+// const person = {
+//     name: "Viktor",
+//     birthYear: 1989
+// }
+
+//person.name = "Maxim";
+
+for (let key in person) {
+  console.log("Key", key, person[key]);
+}
