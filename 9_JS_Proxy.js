542cd434e830a4c5d7223e04f8f4bf62095fc7cb
linostin <lifesins@gmail.com>
2019-12-12 22:58:33 +0300

    9 lesson part 1

--- 9_JS_Proxy.js
+++ 9_JS_Proxy.js
@@ -1,1 +1,52 @@
-console.log("привет!");
+const person = {
+  name: "Viktor",
+  age: 30,
+  job: "Fullstack"
+};
+
+const op = new Proxy(person, {
+  get(target, prop) {
+    console.log("Target", target);
+    console.log("Prop", prop);
+    return target[prop];
+  },
+  set(target, prop, value) {
+    if (prop in target) {
+      target[prop] = value;
+    } else {
+      throw new Error(`No ${prop} field in target`);
+    }
+  },
+  has(target, prop) {
+    return ["age", "name", "job"].includes(prop);
+  }
+});
+
+//Functions
+
+const log = text => `Log: ${text}`;
+
+const fp = new Proxy(log, {
+  apply(target, thisArg, args) {
+    console.log("Calling fn...");
+    return target.apply(thisArg, args).toUpperCase();
+  }
+});
+
+//Classes
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const PersonProxy = new Proxy(Person, {
+  construct(target, args) {
+    console.log("Construct...");
+    return new target(...args);
+  }
+});
+
+const p = new PersonProxy("Maxim", 30);
