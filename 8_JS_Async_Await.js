a2de461caed72d0e385f5b68e3ccf5eaa08e979c
linostin <lifesins@gmail.com>
2019-12-12 21:57:41 +0300

    8 lesson

--- 8_JS_Async_Await.js
+++ 8_JS_Async_Await.js
@@ -1,15 +1,37 @@
-
-
 const delay = ms => {
-    return new Promise(r => setTimeout(() => r(), ms))
-}
+  return new Promise(r => setTimeout(() => r(), ms));
+};
 
 //delay(2000).then(() => console.log("привет!"))
 
-const url = "https://jsonplaceholder.typicode.com/todos"
+const url = "https://jsonplaceholder.typicode.com/todos";
 
-function fetchTodos() {
-    delay(2000).then(() => {
-        fetch(url).then(response => response.json()) 
-    })
+// function fetchTodos() {
+//     console.log("Fetch todo started")
+//     return delay(2000)
+//     .then(() => {
+//        return fetch(url)
+//     }).then(response => response.json())
+// }
+
+// fetchTodos()
+//     .then(data => {
+//         console.log("Data:", data)
+//     })
+//     .catch(e => console.error(e))
+
+//async/await
+
+async function fetchAsyncTodos() {
+  console.log("Fetch todo started");
+  try {
+    await delay(2000);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Data:", data);
+  } catch (e) {
+    console.error(e);
+  }
 }
+
+fetchAsyncTodos();
