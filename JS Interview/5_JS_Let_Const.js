f3312268becae97eaf989bf120c5239c9293d43d
linostin <lifesins@gmail.com>
2019-12-15 21:00:39 +0300

    lesson 5 let const

---
+++ JS Interview/5_JS_Let_Const.js
@@ -0,0 +1,26 @@
+//Let
+//позволяет создавать переменные, которые можно в последствии изменить
+
+// let a = "Variable a";
+// let b = "Variable b";
+
+// //block scope
+// {
+//   a = "New Variable A";
+//   let b = "Local Variable B";
+//   //переменная, которая объявлена let доступна только в рамках текущего scope
+//   console.log("Scope A", a);
+//   console.log("Scope B", b);
+// }
+
+// console.log("A", a);
+// console.log("B", b);
+
+//Const
+//константу мы не можем изменить
+const PORT = 8080;
+//PORT = 2000 нельзя изменить
+const array = ["JS", "is", "awesome"];
+array.push("!");
+array[2] = "super";
+console.log(array);
