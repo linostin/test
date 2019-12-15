0f55f124e9783f73b5366b106b19a87d64586d4f
linostin <lifesins@gmail.com>
2019-12-16 02:16:56 +0300

    lesson 6

--- JS Interview/6_JS_Closures.js
+++ JS Interview/6_JS_Closures.js
@@ -2,5 +2,14 @@
 //когда функция имеет доступ до переменных из вышестоящего scope
 
 function sayHelloTo(name) {
-    const message = ""
+    const message = "Hello" + name
+
+    return function() {
+        console.log(message)
+    }
 }
+
+const helloToVik = sayHelloTo("Vik")
+console.log(helloToVik)
+console.log(helloToVik())
+helloToVik()
