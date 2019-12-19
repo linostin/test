9c25594cfebd4ac825ae69016b71d1ae8362385b
linostin <lifesins@gmail.com>
2019-12-20 01:11:50 +0300

    11 final

---
+++ JS Interview/11_JS_Async.js
@@ -0,0 +1,9 @@
+//асинхронность
+
+const first = () => console.log("First");
+const second = () => console.log("Second");
+const third = () => console.log("Third");
+
+first();
+setTimeout(second, 0);
+third();
