740d6827a89704f93c367982b5367339be9a6a39
linostin <lifesins@gmail.com>
2019-12-20 01:05:11 +0300

    11 lesson async

---
+++ JS Interview/10_JS_Prototype.js
@@ -0,0 +1,26 @@
+//__proto__
+
+// function Cat(name, color) {
+//   this.name;
+//   this.color;
+// }
+
+// Cat.prototype.voice = function() {
+//   console.log(`Cat ${this.name} says meow`);
+// };
+
+// const cat = new Cat("Kot", "white");
+
+// //cat.voice();
+// console.log(Cat.prototype)
+
+function Person() {
+
+}
+
+Person.prototype.legs = 2
+Person.prototype.skin = "white"
+
+const person = "Vladilen"
+
+console.log(person.legs)
