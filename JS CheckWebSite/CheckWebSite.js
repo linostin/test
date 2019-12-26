29bd5bb8ba2bcc3d87fbc3e83e151a0965f433f6
linostin <lifesins@gmail.com>
2019-12-26 23:52:39 +0300

    web

--- JS CheckWebSite/CheckWebSite.js
+++ JS CheckWebSite/CheckWebSite.js
@@ -12,41 +12,41 @@
 // }
 
 
-// let request = new XMLHttpRequest();
+let request = new XMLHttpRequest();
 
-// request.onreadystatechange = function() {
-//   if (this.readyState == 4 && this.status == 200) {
-//     console.log(this.readyState);
-//     console.log(this.status);
-//     // console.log(this.responseText)
-//     console.log("SiteWorks");
-//   } else {
-//     console.log("Site not Works");
-//   }
-// };
+request.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    console.log(this.readyState);
+    console.log(this.status);
+    // console.log(this.responseText)
+    console.log("SiteWorks");
+  } else {
+    console.log("Site not Works");
+  }
+};
 
-// request.open("GET", "https://developer.mozilla.org/", true);
-// request.send();
+request.open("GET", "https://developer.mozilla.org/", true);
+request.send();
 
-// fetch("https://developer.mozilla.org/").then(function(response) {
-//     return response.json();
-//   }).then(function(data) {
-//     console.log(data);
-//   }).catch(function() {
-//     console.log("Booo");
-//   });
+fetch("https://developer.mozilla.org/").then(function(response) {
+    return response.json();
+  }).then(function(data) {
+    console.log(data);
+  }).catch(function() {
+    console.log("Not works");
+  });
 
-
-// fetch('http://ip-api.com/json')
+// http://ip-api.com/json
+// fetch('https://vc.ru/', {mode: "no-cors"})
 //    .then( response => response.json() )
 //    .then( data => console.log(data) )
 
 
 //  let url = "https://jsonplaceholder.typicode.com/";  
 async function fetchAsync (url) {
-    let response = await fetch(url, {mode: "no-cors"});
+    let response = await fetch(url);
     console.log(response)
     let data = await response.json();
     console.log(data)
   }
-  fetchAsync("https://developer.mozilla.org/")
+  fetchAsync("https://vc.ru/")
